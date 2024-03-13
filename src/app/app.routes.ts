b9ed44aa8585a0ecef3d21b86c055390bebdfdba
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-03-13 18:45:26 -0300

    Add Tailwind CSS and PostCSS dependencies, update global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'home',
+    redirectTo: ''
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent)
+  }
+];
---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './about.component.html',
+  styles: ``
+})
+export class AboutComponent {
+
+}
